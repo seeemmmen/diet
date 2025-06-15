272b5ce74d4593006d6aad3928962d29f0d622c2
seeemmmen <seeemmmen@gmail.com>
2025-06-15 22:28:13 +0200

    fixing design

--- src/components/siedebar.tsx
+++ src/components/siedebar.tsx
@@ -1,34 +1,40 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../img/mainpage/logo.svg";
 import home from "../img/app/home.svg";
 import recipes from "../img/app/recipes.svg";
 import progress from "../img/app/progress.svg";
 
-
-
 function Sidebar() {
+    const location = useLocation(); // Получаем текущий маршрут
+
     return (
         <nav className="sidebar">
             <div className="logo">
                 <img src={logo} alt="Логотип" />
             </div>
             <ul>
-                <li className="active">
+                <li className={location.pathname === "/app" ? "active" : ""}>
                     <Link to="/app">
-                        <i className="fas fa-home"></i>
-                        <span><img src={home} alt=""/>Home</span>
+                        <span>
+                            <img src={home} alt="Иконка Главная" />
+                            Home
+                        </span>
                     </Link>
                 </li>
-                <li>
+                <li className={location.pathname === "/app/recipes" ? "active" : ""}>
                     <Link to="/app/recipes">
-                        <i className="fas fa-utensils"></i>
-                        <span><img src={recipes} alt=""/>Recipes</span>
+                        <span>
+                            <img src={recipes} alt="Иконка Рецепты" />
+                            Recipes
+                        </span>
                     </Link>
                 </li>
-                <li>
+                <li className={location.pathname === "/app/progress" ? "active" : ""}>
                     <Link to="/app/progress">
-                        <i className="fas fa-chart-line"></i>
-                        <span><img src={progress} alt=""/>Progress</span>
+                        <span>
+                            <img src={progress} alt="Иконка Прогресс" />
+                            Progress
+                        </span>
                     </Link>
                 </li>
             </ul>
